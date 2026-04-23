f900cb8756b63e7dc70d74f292775f25708d7827
Darck-Lost <darcklost1987@gmail.com>
2026-04-23 14:08:11 -0600

    Actualización de Html, CSS y JS Mejora de animaciones

--- script.js
+++ script.js
@@ -11,3 +11,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
     elements.forEach(el => observer.observe(el));
 });
+
+const glow = document.createElement("div");
+glow.classList.add("cursor-glow");
+document.body.appendChild(glow);
+
+document.addEventListener("mousemove", e => {
+    glow.style.left = e.clientX + "px";
+    glow.style.top = e.clientY + "px";
+});
